455658a69574f690a2c49c804ba06ec0b08ab247
FearleSS-2210 <80671139+FearleSS-2210@users.noreply.github.com>
2021-04-30 19:48:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,12 +16,12 @@ function getmenu()
 
 function add_item() {
    var htmldata;
-   var item-document.getElementById("add_item").value;
+   var item=document.getElementById("add_item").value;
    menu_list_array.push(item);
    menu_list_array.sort();
    htmldata="<section class='card'>"
 
-   for (var 1=0;i<menu_list_array.length;i++)
+   for (var i=0;i<menu_list_array.length;i++)
    {
        htmldata=htmldata+'<div class = "card">' + '<img src = "images/pizzaImg.png"/>' + menu_list_array[i] + '</div>'
    }
